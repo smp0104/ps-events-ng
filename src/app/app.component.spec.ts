878704841ca3cdfd03b03230d7ef2e86f5160202
Ku <Manikanta_Praveen_Ku@dell.com>
2018-10-21 21:27:18 +0530

    Changed root selector and Bootstrap compontent to events

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {  EventsAppComponent } from './events-app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        EventsAppComponent
       ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent( EventsAppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'ps-events-ng'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent( EventsAppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ps-events-ng');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent( EventsAppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ps-events-ng!');
