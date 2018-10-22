17047befd431f14d63fcd0a9c757eb75b01b881c
Ku <Manikanta_Praveen_Ku@dell.com>
2018-10-23 01:11:09 +0530

    > added list and thumbnail components >Basic Communication using Input Property

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: ['./events-list.component.css']
+})
+export class EventsListComponent implements OnInit {
+  events = {
+    name: 'Angular Connect',
+    date: '9/26/2036',
+    time: '10:00 am',
+    price: 649.99,
+    imageUrl: '/assets/images/basic-shield.png',
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'England'
+    }
+};
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
